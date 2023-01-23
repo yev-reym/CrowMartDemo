66583c6e52c57a6b5e24e8d6aea28d054b9e9958
yev-reym <yevgeniyreym@gmail.com>
2023-01-23 00:17:52 -0500

    fix: rendering to not reload

--- crowmart/scripts/cart/index.ts
+++ crowmart/scripts/cart/index.ts
@@ -1,6 +1,232 @@
 'use strict'
 
 import { fireView } from '../analytics/fireView.js'
+export class CartManager {
+  public state
+  public currentCartState!: CrowMarket.CartStore
+
+
+  public static isCartPage(): boolean {
+    return globalThis?.location?.pathname === '/cart.html'
+  }
+
+  public static isItemsPage(): boolean {
+    return ['/accessories.html', '/crows.html'].includes(
+      globalThis?.location?.pathname
+    )
+  }
+
+
+  constructor(state: State) {
+    this.state = state
+  }
+  
+  public init(stateSnapshot: CrowMarket.CrowMarketState): void {
+    this.currentCartState = stateSnapshot.cart
+
+    if (CartManager.isItemsPage()) {
+      this.mountAddToCartListeners()
+    }
+
+    if (CartManager.isCartPage()) {
+      //Manipulate DOM to render Cart State info to user
+      this.renderCartInfo()
+    }
+  }
+
+  private mountAddToCartListeners(): void {
+    const cartListings = document.querySelectorAll("div[id$='listing']")
+
+    if (cartListings?.length) {
+      for (let listing of cartListings) {
+        const itemId = listing.getAttribute(
+          'data-item-id'
+        ) as keyof typeof CART_ITEM_MAP
+
+        listing.addEventListener('click', (event) => {
+          event.preventDefault()
+          event.stopPropagation()
+          this.addItemToCart(itemId)
+          fireView('cart_click')
+        })
+      }
+    }
+  }
+
+
+
+  /**Handle Rendering Logic based on state */
+  private renderCartInfo(): void {
+    this.renderCartItems()
+    this.renderTotal()
+  }
+
+  private renderTotal(): void {
+    const totalElement = document.getElementById('total') as Element
+    const countElement = document.getElementById('count') as Element
+    const { total, items } = this.currentCartState
+    const count = items.reduce((acc, { quantity }): number => {
+      return (acc += quantity)
+    }, 0)
+
+    totalElement.innerHTML = `Total: $${(total / 100).toFixed(2)}`
+    countElement.innerHTML = `You have ${count} items in cart`
+  }
+
+  private renderCartItems(): void {
+    const { items } = this.currentCartState
+    const elementToHook = document.getElementById('cart-items') as HTMLElement
+
+    items.forEach((cartItem) => {
+      this.renderCartItem(cartItem, elementToHook)
+    })
+
+    this.addRemoveCartListeners()
+  }
+
+  private renderCartItem(
+    cartItem: CrowMarket.CartItem,
+    elementToHook: HTMLElement
+  ): void {
+    const { name, id, quantity, price } = cartItem
+    const imgSrc = 'images/' + name.replace(' ', '_').toLowerCase() + '.jpg'
+
+    const htmlAsText = `<div>
+                            <img class="u-max-full-width" src=${imgSrc}>
+                            <h4>${name}</h4><h5>${this.getItemPriceText(price,quantity)}</h5>
+                            <button data-item-id=${id} class="button-primary">Remove Item from Cart</button>
+                        </div>`
+
+    elementToHook.insertAdjacentHTML('afterbegin', htmlAsText)
+  }
+
+  private getItemPriceText(price: number, quantity: number): string {
+    const quantityText = quantity === 1 ? '' : ` x ${quantity}`
+
+    return `$${(price / 100).toFixed(2)}${quantityText}`
+  }
+
+  private addRemoveCartListeners(): void {
+    const removeCartButtons = document.querySelectorAll('button.button-primary')
+
+    if (removeCartButtons?.length) {
+      for (let button of removeCartButtons) {
+        const itemId = button.getAttribute(
+          'data-item-id'
+        ) as keyof typeof CART_ITEM_MAP
+
+        button.addEventListener('click', (event) => {
+          event.stopPropagation()
+          event.preventDefault()
+          this.removeItemFromCart(itemId)
+        })
+      }
+    }
+  }
+
+
+  /**
+   *
+   * Cart CRUD
+   */
+  private async addItemToCart(
+    itemId: keyof typeof CART_ITEM_MAP
+  ): Promise<void> {
+    const newCartItems = [...this.currentCartState.items]
+    let newTotal = this.currentCartState.total
+
+    // Create an updated state using current cart state
+    const itemToAdd = CART_ITEM_MAP[itemId]
+    const itemIndexInCart = newCartItems.findIndex(
+      (cartItem) => cartItem.id === itemId
+    )
+
+    if (itemIndexInCart !== -1) {
+      //If item already exists in cart, all we do is update the quantity
+      const currentItemInCart = newCartItems[itemIndexInCart]
+      const currentQuantity = currentItemInCart.quantity
+
+      newCartItems[itemIndexInCart] = {
+        ...currentItemInCart,
+        quantity: currentQuantity + 1
+      }
+    } else {
+      // Add new item to cart
+      newCartItems.push({ ...itemToAdd, id: itemId })
+    }
+
+    newTotal += itemToAdd.price
+
+    // Upsert new state to reflect changes
+    const newCartState = {
+      items: newCartItems,
+      total: newTotal
+    }
+
+    await this.state.upsertToState('cart', newCartState)
+
+    this.currentCartState = newCartState
+  }
+
+  private async removeItemFromCart(
+    itemId: keyof typeof CART_ITEM_MAP
+  ): Promise<void> {
+    //Find item to delete
+    const cartItemToDelete = this.currentCartState.items.find(
+      (item: CrowMarket.CartItem): boolean => item.id === itemId
+    ) as CrowMarket.CartItem
+
+    let newItemsState
+    const newTotal = this.currentCartState.total - cartItemToDelete.price
+
+    //If only one of item, remove from state, otherwise update quantity
+    if (cartItemToDelete.quantity === 1) {
+      newItemsState = this.currentCartState.items.filter(
+        (item: CrowMarket.CartItem): boolean => item.id !== itemId
+      )
+    } else {
+      newItemsState = this.currentCartState.items.map((item) =>
+        item.id === itemId
+          ? { ...cartItemToDelete, quantity: cartItemToDelete.quantity - 1 }
+          : item
+      )
+    }
+
+    // Insert new state to reflect changes
+    const newCartState = {
+      items: newItemsState,
+      total: newTotal
+    }
+
+    await this.state.upsertToState('cart', newCartState)
+
+    this.currentCartState = newCartState
+
+    this.updateItemsView()
+  }
+
+  private updateItemsView(): void {
+      const cartItemsInDOM = document.getElementById('cart-items')?.childNodes || []
+      const cartItemsInState = this.currentCartState.items.reduce((itemMap: { [id: string]: string}, item: CrowMarket.CartItem) => {
+        const { id, quantity, price } = item
+          itemMap[id] = this.getItemPriceText(price, quantity)
+          return itemMap
+      }, {})
+
+      cartItemsInDOM.forEach((node) => {
+        const buttonNode = Array.from(node.childNodes).find(n => n.nodeName === 'BUTTON') as Element
+        const itemId = buttonNode.getAttribute('data-item-id')
+
+        //Remove node if the associated cart item id does not exist in the current state
+        if (itemId && !cartItemsInState[itemId]) {
+          node.parentNode?.removeChild(node)
+        } else {
+          const priceInfoNode = Array.from(node.childNodes).find(n => n.nodeName === 'H5') as Element
+          priceInfoNode.innerHTML = cartItemsInState[itemId as string]
+        }
+      })
+  }
+}
 
 const CART_ITEM_MAP = {
   '09f8d37e': {
@@ -68,203 +294,5 @@ const CART_ITEM_MAP = {
     subscription: false,
     available: true,
     item_position_index: 2
-  }
-}
-
-export class CartManager {
-  public state
-  public currentCartState!: CrowMarket.CartStore
-
-  public static isCartPage(): boolean {
-    return globalThis?.location?.pathname === '/cart.html'
-  }
-
-  public static isItemsPage(): boolean {
-    return ['/accessories.html', '/crows.html'].includes(
-      globalThis?.location?.pathname
-    )
-  }
-
-  constructor(state: State) {
-    this.state = state
-  }
-
-  public init(stateSnapshot: CrowMarket.CrowMarketState): void {
-    this.currentCartState = stateSnapshot.cart
-
-    if (CartManager.isItemsPage()) {
-      this.mountAddToCartListeners()
-    }
-
-    if (CartManager.isCartPage()) {
-      //Manipulate DOM to render Cart State info to user
-      this.renderCartInfo()
-    }
-  }
-
-  private mountAddToCartListeners(): void {
-    const cartListings = document.querySelectorAll("div[id$='listing']")
-
-    if (cartListings?.length) {
-      for (let listing of cartListings) {
-        const itemId = listing.getAttribute(
-          'data-item-id'
-        ) as keyof typeof CART_ITEM_MAP
-
-        listing.addEventListener('click', (event) => {
-          event.preventDefault()
-          event.stopPropagation()
-          this.addItemToCart(itemId)
-          fireView('cart_click')
-        })
-      }
-    }
-  }
-
-  /**Handle Rendering Logic based on state */
-  private renderCartInfo(): void {
-    this.renderCartItems()
-    this.renderTotal()
-  }
-
-  private renderTotal(): void {
-    const totalElement = document.getElementById('total') as Element
-    const countElement = document.getElementById('count') as Element
-    const { total, items } = this.currentCartState
-    const count = items.reduce((acc, { quantity }): number => {
-      return (acc += quantity)
-    }, 0)
-
-    totalElement.innerHTML = `Total: $${(total / 100).toFixed(2)}`
-    countElement.innerHTML = `You have ${count} items in cart`
-  }
-
-  private renderCartItems(): void {
-    const { items } = this.currentCartState
-    const elementToHook = document.getElementById('cart-items') as HTMLElement
-
-    items.forEach((cartItem) => {
-      this.renderCartItem(cartItem, elementToHook)
-    })
-
-    this.addRemoveCartListeners()
-  }
-
-  private addRemoveCartListeners(): void {
-    const removeCartButtons = document.querySelectorAll('button.button-primary')
-
-    if (removeCartButtons?.length) {
-      for (let button of removeCartButtons) {
-        const itemId = button.getAttribute(
-          'data-item-id'
-        ) as keyof typeof CART_ITEM_MAP
-
-        button.addEventListener('click', (event) => {
-          event.stopPropagation()
-          event.preventDefault()
-          this.removeItemFromCart(itemId)
-        })
-      }
-    }
-  }
-
-  private renderCartItem(
-    cartItem: CrowMarket.CartItem,
-    elementToHook: HTMLElement
-  ): void {
-    const { name, id, quantity, price } = cartItem
-    const imgSrc = 'images/' + name.replace(' ', '_').toLowerCase() + '.jpg'
-    const quantityText = quantity === 1 ? '' : ` x ${quantity}`
-    const htmlAsText = `
-        <div>
-            <img class="u-max-full-width" src=${imgSrc}>
-            <h4>${name}</h4><h5>$${(price / 100).toFixed(2)}${quantityText}</h5>
-            <button data-item-id=${id} class="button-primary">Remove Item from Cart</button>
-        </div>
-        `
-
-    elementToHook.insertAdjacentHTML('afterbegin', htmlAsText)
-  }
-
-  /**
-   *
-   * Cart CRUD
-   */
-  private async addItemToCart(
-    itemId: keyof typeof CART_ITEM_MAP
-  ): Promise<void> {
-    const newCartItems = [...this.currentCartState.items]
-    let newTotal = this.currentCartState.total
-
-    // Create an updated state using current cart state
-    const itemToAdd = CART_ITEM_MAP[itemId]
-    const itemIndexInCart = newCartItems.findIndex(
-      (cartItem) => cartItem.id === itemId
-    )
-
-    if (itemIndexInCart !== -1) {
-      //If item already exists in cart, all we do is update the quantity
-      const currentItemInCart = newCartItems[itemIndexInCart]
-      const currentQuantity = currentItemInCart.quantity
-
-      newCartItems[itemIndexInCart] = {
-        ...currentItemInCart,
-        quantity: currentQuantity + 1
-      }
-    } else {
-      // Add new item to cart
-      newCartItems.push({ ...itemToAdd, id: itemId })
-    }
-
-    newTotal += itemToAdd.price
-
-    // Upsert new state to reflect changes
-    const newCartState = {
-      items: newCartItems,
-      total: newTotal
-    }
-
-    await this.state.upsertToState('cart', newCartState)
-
-    this.currentCartState = newCartState
-  }
-
-  private async removeItemFromCart(
-    itemId: keyof typeof CART_ITEM_MAP
-  ): Promise<void> {
-    //Find item to delete
-    const cartItemToDelete = this.currentCartState.items.find(
-      (item: CrowMarket.CartItem): boolean => item.id === itemId
-    ) as CrowMarket.CartItem
-
-    let newItemsState
-    const newTotal = this.currentCartState.total - cartItemToDelete.price
-    console.log('total', newTotal)
-
-    //If only one of item, remove from state, otherwise update quantity
-    if (cartItemToDelete.quantity === 1) {
-      newItemsState = this.currentCartState.items.filter(
-        (item: CrowMarket.CartItem): boolean => item.id !== itemId
-      )
-    } else {
-      newItemsState = this.currentCartState.items.map((item) =>
-        item.id === itemId
-          ? { ...cartItemToDelete, quantity: cartItemToDelete.quantity - 1 }
-          : item
-      )
-    }
-
-    // Upsert new state to reflect changes
-    const newCartState = {
-      items: newItemsState,
-      total: newTotal
-    }
-
-    await this.state.upsertToState('cart', newCartState)
-
-    this.currentCartState = newCartState
-
-    //Quick way of updating page, not ideal though
-    document.location.reload()
   }
 }
